3b6793289f2a8006037f7c586498df75fd502c0d
David Malary <davidmalary@gmail.com>
2016-01-25 00:16:52 -0500

    scraper finished

--- server.js
+++ server.js
@@ -16,6 +16,43 @@ console.log('Silence please...' + '\n' + 'Curtains up...' + '\n' + 'Server start
 
 app.get('/scrape', function(req, res){
 
+  url = 'http://www.imdb.com/title/tt1229340/';
+
+  request(url, function(err, res, html){
+    if (!err){
+      var $ = cheerio.load(html);
+
+      var title, release, rating;
+      var json = {
+        title : "",
+        release : "",
+        rating : ""
+      };
+
+      $('.header').filter(function(){
+        var data = $(this);
+
+        title = data.children().first().text();
+        release = data.children().last().children().text();
+
+        json.title = title;
+        json.release = release;
+      });
+
+      $('.star-box-giga-star').filter(function(){
+        var data = $(this);
+
+        rating = data.text();
+        json.rating = rating;
+      })
+    }; // end of if
+
+    fs.write('output.json', JSON.stringify(json, null, 4), function(err){
+      console.log('File created! JSON file located in project dir')
+    });
+
+    res.send('Check console for status');
+  }) // end of request
 });
 
 exports = module.exports = app;
